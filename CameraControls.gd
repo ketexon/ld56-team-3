169efa3aced5d43f811fe21d016d02d3280081bd
ketexon <zane.a.s.clark@gmail.com>
2024-10-04 20:33:10 -0700

    finished camera movement

--- CameraControls.gd
+++ CameraControls.gd
@@ -4,64 +4,75 @@ extends Camera2D
 @export var colony_view_zoom = 20.0
 @export var map_view_zoom = 1.0
 @export var zoom_duration = 1.0
-@export var zoom_curve: Curve
+@export var pan_speed: float = 10.0
+@export var scroll_speed: float = 10.0
+@export var scroll_sensitivity: float = 0.01
 
 var start_cursor_pos: Vector2
 var start_pos: Vector2
 
-var zooming = false
-var zoom_start: float = colony_view_zoom
 var zoom_goal: float = colony_view_zoom
-var zoom_t: float = 0
+
+var last_zoom_level: int = -1
+
+var zoom_percent: float = 0
+
+@onready var pan_goal: Vector2 = position
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
+		var mods = event.get_modifiers_mask().shift_pressed
+		if event.button_index == MOUSE_BUTTON_LEFT and not mods.shift_pressed:
 			if event.pressed:
 				start_cursor_pos = get_viewport().get_mouse_position()
 				start_pos = position
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			set_zoom_percent(zoom_percent + scroll_sensitivity)
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			set_zoom_percent(zoom_percent - scroll_sensitivity)
+
 	if event.is_action_pressed("center_on_colony"):
 		center_on_player()
+
+	var cur_zoom_level = null
 	if event.is_action_pressed("zoom_0"):
 		set_zoom_percent(0)
+		cur_zoom_level = 0
 	if event.is_action_pressed("zoom_1"):
 		set_zoom_percent(0.33)
+		cur_zoom_level = 1
 	if event.is_action_pressed("zoom_2"):
 		set_zoom_percent(0.66)
+		cur_zoom_level = 2
 	if event.is_action_pressed("zoom_3"):
 		set_zoom_percent(1)
+		cur_zoom_level = 3
+
+	if cur_zoom_level != null:
+		if cur_zoom_level == last_zoom_level:
+			center_on_player()
+		last_zoom_level = cur_zoom_level
 
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		var cur_pos := get_viewport().get_mouse_position()
 		var delta_pos := cur_pos - start_cursor_pos
-		position = start_pos - delta_pos
+		pan_goal = start_pos - delta_pos/zoom.x
 
-	if zooming:
-		zoom_t += _delta
-		if zoom_t < zoom_duration:
-			var t = zoom_curve.sample(zoom_t / zoom_duration)
-			zoom.x = lerpf(
-				zoom_start,
-				zoom_goal,
-				t
-			);
-			zoom.y = zoom.x
+	zoom.x = lerpf(
+		zoom.x,
+		zoom_goal,
+		delta * scroll_speed
+	);
+	zoom.y = zoom.x
 
-			position = Colony.player_colony.position
-		else:
-			zoom.x = zoom_goal
-			zoom.y = zoom.x
-			zooming = false
-
+	position = lerp(position, pan_goal, delta * pan_speed)
 
 
 func set_zoom_percent(percent: float):
-	zooming = true
-	zoom_t = 0
-	zoom_start = zoom.x
-	zoom_goal = lerp(colony_view_zoom, map_view_zoom, percent)
+	zoom_percent = clampf(percent, 0, 1)
+	zoom_goal = lerp(colony_view_zoom, map_view_zoom, sqrt(zoom_percent))
 
 func center_on_player():
-	pass
+	pan_goal = Colony.player_colony.position
