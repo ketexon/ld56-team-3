35c73557ff4967bb3e8eb81b857baf26579f1558
kennedy kang <kennedyekang@gmail.com>
2024-10-05 15:49:41 -0700

    update shopitem

--- Shop/ShopItem.gd
+++ Shop/ShopItem.gd
@@ -3,4 +3,19 @@ extends Resource
 
 @export var name: String
 @export var texture: Texture2D
-@export var 
+
+@export var cost_wood: int
+@export var cost_mushroom: int
+@export var cost_jewel: int
+
+@export var min_power: int
+@export var delta_power: int
+@export var mult_power: int = 1
+
+@export var min_persuasion: int
+@export var delta_persuasion: int
+@export var mult_persuasion: int = 1
+
+@export var min_reputation: int
+@export var delta_reputation: int
+@export var mult_reputation: int = 1 
