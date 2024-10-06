f6d1d0d86a11883a96d919e627024432ef8f6d75
Caleb Kim <calebmk21@gmail.com>
2024-10-06 13:05:06 -0700

    achievement framework?

--- Achievements/achievement_manager.gd
+++ Achievements/achievement_manager.gd
@@ -1,11 +1,36 @@
 extends Node2D
 
+# Variables tracking achievement progress
+var completedAchievements : int = 0
+var powerAchievements : int = 0
+var persuasionAchievements : int = 0
+var reputationAchievements : int = 0
+var funnyAchievements : int = 0
+var devAchievements : int = 0
+
+# Signals
+signal achieved(name)
+
+# Achievement Unlock Function
+func achievementGet(achievement : String) -> void:
+	achieved.emit(achievement)
+
+# Displays UI for an Achivement Unlock
+func displayAchievement(achievement : String) -> void:
+	pass
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var timer = get_node("Timer")
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if (player_colony.power == 10):
+		pass
+
+
+func _on_signal_test_pressed() -> void:
+	achieved.emit("name")
