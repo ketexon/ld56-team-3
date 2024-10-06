3ad28509909cab6717c63a44aae271bc436f1a5d
kennedy kang <kennedyekang@gmail.com>
2024-10-05 21:45:58 -0700

    shop button spawn in now

--- Shop/ShopButton.gd
+++ Shop/ShopButton.gd
@@ -1,12 +1,21 @@
 class_name ShopButton
 extends Button
 
+@export var shop_item: ShopItem
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_ShopButton_pressed() -> void:
+	if Colony.player_colony.buy_shop_item(shop_item): queue_free()
+
+func initialize(shop_item: ShopItem) -> void:
 	pass
