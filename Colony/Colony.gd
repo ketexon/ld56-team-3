518f64c02b7b437bd35e38544a5bcb1d67fabbc7
ketexon <zane.a.s.clark@gmail.com>
2024-10-05 17:59:58 -0700

    added colony radius

--- Colony/Colony.gd
+++ Colony/Colony.gd
@@ -9,6 +9,11 @@ extends Node2D
 @export var monarch: TinyCreature
 @export var tiny_creatures: Array[TinyCreature]
 
+var radius: float:
+	get:
+		var n = len(tiny_creatures)
+		return 256 * pow(1.5, n/32.0)
+
 static var player_colony: Colony
 
 func _ready() -> void:
