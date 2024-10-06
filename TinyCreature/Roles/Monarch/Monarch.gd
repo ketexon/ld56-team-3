1409ed1a73c4e57a3862ea04cf33b32e5e1ab19c
ketexon <zane.a.s.clark@gmail.com>
2024-10-05 20:57:03 -0700

    made monarch work without being player

--- TinyCreature/Roles/Monarch/Monarch.gd
+++ TinyCreature/Roles/Monarch/Monarch.gd
@@ -1,23 +1,26 @@
 class_name Monarch
 extends TinyCreatureRole
 
-@onready var destination: Vector2 = global_position
+@onready var destination: Vector2:
+	set(new_dest):
+		if not enabled: return
+		destination = new_dest
+		var disp = new_dest - tiny_creature.global_position
+		if not disp.is_zero_approx():
+			moving = true
+			base_movement_ai.enabled = false
+
+			tiny_creature.running = false
+			tiny_creature.movement_dir = disp
 
 var moving := false
 
 func _unhandled_input(event: InputEvent) -> void:
 	if not enabled: return
+	if not tiny_creature.colony.player: return
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_RIGHT and event.is_pressed():
 			destination = get_global_mouse_position()
-			var disp = destination - tiny_creature.global_position
-			if not disp.is_zero_approx():
-				moving = true
-				base_movement_ai.enabled = false
-
-				tiny_creature.running = false
-				tiny_creature.movement_dir = disp
-
 
 func _process(delta: float) -> void:
 	if not enabled: return
