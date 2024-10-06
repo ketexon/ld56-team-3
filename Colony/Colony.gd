b8ce26667d04bdadb64a2367852027ed9e1b4fec
kennedy kang <kennedyekang@gmail.com>
2024-10-05 18:48:45 -0700

    shop stuff erm

--- Colony/Colony.gd
+++ Colony/Colony.gd
@@ -9,6 +9,13 @@ extends Node2D
 @export var monarch: TinyCreature
 @export var tiny_creatures: Array[TinyCreature]
 
+@export var  available_shop_items: Array[ShopItem]
+
+@export var wood: int
+@export var mushrooms: int
+@export var jewels: int
+
+
 var radius: float:
 	get:
 		var n = len(tiny_creatures)
@@ -19,3 +26,20 @@ static var player_colony: Colony
 func _ready() -> void:
 	if player:
 		player_colony = self
+
+func buy_shop_item(shop_item:ShopItem) -> bool:
+	if wood < shop_item.cost_wood:
+		return false
+	if mushrooms < shop_item.cost_mushroom:
+		return false
+	if jewels < shop_item.cost_jewel:
+		return false
+	if power < shop_item.min_power:
+		return false
+	if persuasion < shop_item.min_persuasion:
+		return false
+	if reputation < shop_item.min_reputation:
+		return false
+	return true
+
+	
